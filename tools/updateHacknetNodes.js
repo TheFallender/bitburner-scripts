7821a1a971382e538b4c47b19632b2bd6965bc8c
TheFallender <a.ferrari@thefallen.io>
2023-07-13 16:25:23 +0200

    Hacknet nodes now include the option to show cost

--- tools/updateHacknetNodes.js
+++ tools/updateHacknetNodes.js
@@ -31,12 +31,20 @@ const keywordsList = [
         'The cores the servers should have.',
         KEYWORD_FLAGS.REQUIRED,
         KEYWORD_FLAGS.PAIR
+    ),
+    new KeywordArgument(
+        ['-m', '--cost', '--money'],
+        'moneyCost',
+        'The money cost of the upgrade to the servers.',
+        KEYWORD_FLAGS.NOT_REQUIRED,
+        KEYWORD_FLAGS.NO_PAIR
     )
 ];
 
 // Max things
 const MAX_LEVEL = 200;
 const MAX_RAM = 64;
+const MAX_CORES = 16;
 
 
 /** @param {NS} ns */
@@ -45,33 +53,81 @@ export async function main(ns) {
     const keywordArgs = new Arguments(ns, keywordsList);
     if (!keywordArgs.valid) return;
 
-    // Remove the servers under our max ram
-    for (let i = 0; i < keywordArgs.args.servers; i++) {
-        // Check if it needs to buy a server
-        if (i >= ns.hacknet.numNodes()) {
-            if (ns.hacknet.purchaseNode() === -1)
-                return ns.tprint('Failed to purchase a server.');
-        }
+    let upgradeCost = 0;
 
+    let ceilServers = keywordArgs.args.servers - ns.hacknet.numNodes();
+    if (ceilServers > ns.hacknet.maxNumNodes())
+        ceilServers = ns.hacknet.maxNumNodes() - ns.hacknet.numNodes();
+
+    for (let i = 0; i < ceilServers; i++) {
+        if (keywordArgs.args.moneyCost)
+            upgradeCost += ns.hacknet.getPurchaseNodeCost() * (1.85**i);
+        else if (ns.hacknet.purchaseNode() === -1)
+            return ns.tprint('Failed to purchase a server.');
+    }
+
+    // Uprade the servers
+    for (let i = 0; i < keywordArgs.args.servers && i < ns.hacknet.numNodes() && ceilServers == 0; i++) {
         // Check if it needs to upgrade the server
         const nodeInfo = ns.hacknet.getNodeStats(i);
 
         // Level Check
         if (nodeInfo.level < keywordArgs.args.level) {
-            const levelUpgrades = keywordArgs.args.level - nodeInfo.level;
-            ns.hacknet.upgradeLevel(i, levelUpgrades);
+            const ceilUpgrade = keywordArgs.args.level > MAX_LEVEL ? MAX_LEVEL : keywordArgs.args.level;
+            const upgradesNum = ceilUpgrade - nodeInfo.level;
+            if (keywordArgs.args.moneyCost)
+                upgradeCost += ns.hacknet.getLevelUpgradeCost(i, upgradesNum);
+            else
+                ns.hacknet.upgradeLevel(i, upgradesNum);
         }
 
         // Ram Check
         if (nodeInfo.ram < keywordArgs.args.ram) {
-            const ramUpgrades = Math.ceil(Math.log2(keywordArgs.args.ram) - Math.log2(nodeInfo.ram));
-            ns.hacknet.upgradeRam(i, ramUpgrades);
+            const ceilUpgrade = keywordArgs.args.ram > MAX_RAM ? MAX_RAM : keywordArgs.args.ram;
+            const upgradesNum = Math.ceil(Math.log2(ceilUpgrade) - Math.log2(nodeInfo.ram));
+            if (keywordArgs.args.moneyCost)
+                upgradeCost += ns.hacknet.getRamUpgradeCost(i, upgradesNum);
+            else
+                ns.hacknet.upgradeRam(i, upgradesNum);
         }
 
         // Core Check
         if (nodeInfo.cores < keywordArgs.args.cores) {
-            const coreUpgrades = keywordArgs.args.cores - nodeInfo.cores;
-            ns.hacknet.upgradeCore(i, coreUpgrades);
-        }   
+            const ceilUpgrade = keywordArgs.args.cores > MAX_CORES ? MAX_CORES : keywordArgs.args.cores;
+            const upgradesNum = ceilUpgrade - nodeInfo.cores;
+            if (keywordArgs.args.moneyCost)
+                upgradeCost += ns.hacknet.getCoreUpgradeCost(i, upgradesNum);
+            else
+                ns.hacknet.upgradeCore(i, upgradesNum);
+        }
     }
+
+    // Print the cost
+    if (keywordArgs.args.moneyCost) {
+        let printCostStr = '';
+        if (keywordArgs.args.servers > ns.hacknet.numNodes())
+            printCostStr = 'Purchase Cost of the new servers (no upgrades included): ' + getSuffixOfCost(upgradeCost);
+        else
+            printCostStr = 'Upgrade Cost of the current servers ' + getSuffixOfCost(upgradeCost);
+        ns.tprintf(printCostStr);
+    }
+}
+
+function getSuffixOfCost(cost) {
+    const dictionaryOfSuffixes = {
+        0: '',
+        1: 'k',
+        2: 'm',
+        3: 'b'
+    }
+
+    let suffix = 0;
+    while (cost > 1000) {
+        cost /= 1000;
+        suffix++;
+    }
+
+    cost = Math.round(cost * 1000) / 1000;
+
+    return "$" + cost + dictionaryOfSuffixes[suffix];
 }
